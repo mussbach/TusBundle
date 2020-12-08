7b812d28690526907a6f687b844ce2083cc9460e
eFrane <efrane@meanderingsoul.com>
2020-12-08 12:33:27 +0100

    Initial commit

---
+++ src/Bundle/TusBundle.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+/**
+ * @copyright 2020
+ * @author Stefan "eFrane" Graupner <efrane@meanderingsoul.com>
+ */
+
+namespace EFrane\TusBundle\Bundle;
+
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class TusBundle extends Bundle
+{
+
+}
